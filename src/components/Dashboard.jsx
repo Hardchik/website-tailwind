1b498afe54cab2292b1214bcebebf677cc0848eb
Vishisht Chhabra <chhabrahardik345@gmail.com>
2023-04-19 02:22:32 +0530

    functionality added for mobile view

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -154,40 +154,55 @@ export default function Dashboard() {
                           </ul>
                         </li>
                         {documents.length===0?<></>:
-                            <li>
-                                <div className="text-xs font-semibold leading-6 text-gray-400">Your documents</div>
-                                <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                {documents.map((doc) => (
-                                    <li key={doc.id}>
-                                    <a
-                                    //   href={doc.href}
-                                        onClick={() => setMenuOptions(doc.id)}
-                                        className={classNames(
-                                        menuOptions === doc.id
-                                            ? 'bg-gray-50 text-indigo-600'
-                                            : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                                        'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                                        )}
-                                    >
-                                        <span
-                                        className={classNames(
-                                            menuOptions === doc.id
-                                            ? 'text-indigo-600'
-                                            : 'text-gray-400 group-hover:text-indigo-600',
-                                            'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg text-[0.625rem] font-medium bg-white'
-                                        )}
-                                        >
-                                        {/* {team.initial} */}
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-                                            </svg>
-                                        </span>
-                                        <span className="truncate">{doc.value}</span>
-                                    </a>
-                                    </li>
-                                ))}
-                                </ul>
-                            </li>
+                              <li>
+                                  <div className="text-xs font-semibold leading-6 text-gray-400">Your documents</div>
+                                  <ul role="list" className="-mx-2 mt-2 space-y-1">
+                                  {documents.map((doc) => (
+                                      <li key={doc.id}>
+                                      <a
+                                      //   href={doc.href}
+                                          onClick={() => setMenuOptions(doc.id)}
+                                          className={classNames(
+                                          menuOptions === doc.id
+                                              ? 'bg-gray-50 text-indigo-600'
+                                              : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
+                                          'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                                          )}
+                                      >
+                                          <span
+                                          className={classNames(
+                                              menuOptions === doc.id
+                                              ? 'text-indigo-600'
+                                              : 'text-gray-400 group-hover:text-indigo-600',
+                                              'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg text-[0.625rem] font-medium bg-white'
+                                          )}
+                                          >
+                                          {/* {team.initial} */}
+                                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                              </svg>
+                                          </span>
+                                          <div className='flex justify-between w-full'>
+                                            {menuOptions === doc.id && edit===true?(
+                                            <input 
+                                            value={changeDocument} 
+                                            placeholder='New document' onChange={(e)=> setChangeDocument(e.target.value)} />)
+                                            :(<span className="truncate">{doc.value}</span>)}
+                                            {menuOptions === doc.id?(<span className='g-4 flex'>
+                                              <svg onClick={() => {setChangeDocument(doc.value); EditHandler();}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                              </svg>
+
+                                              <svg onClick={DeleteHandler} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                              </svg>
+                                            </span>):(<></>)}
+                                          </div>
+                                      </a>
+                                      </li>
+                                  ))}
+                                  </ul>
+                              </li>
                         }
                         <li>
                           <div className="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
